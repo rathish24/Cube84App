c3fb818931fcd8da196778d182622c4a38a1d73a
Rathish <rathishk24@gmail.com>
2019-10-28 20:13:32 +0530

    Inital Commit

---
+++ app/src/main/java/com/cube84/district360/MainActivity.java
@@ -0,0 +1,321 @@
+package com.cube84.district360;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.DownloadListener;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webview;
+    ImageView logo;
+    ProgressBar bar;
+    TextView version;
+    private String url = "https://bid-support-developer-edition.na122.force.com/";
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private long size = 0;
+
+    public static final int INPUT_FILE_REQUEST_CODE = 1;
+    public static final String EXTRA_FROM_NOTIFICATION = "EXTRA_FROM_NOTIFICATION";
+
+    private WebView mWebView;
+    private ValueCallback<Uri[]> mFilePathCallback;
+    private String mCameraPhotoPath;
+    // Storage Permissions variables
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState != null) {
+            // Restore the previous URL and history stack
+            webview.restoreState(savedInstanceState);
+        }
+        verifyStoragePermissions(this);
+
+        init();
+         process();
+    }
+    private void init() {
+        webview = (WebView) findViewById(R.id.webView);
+        logo = (ImageView) findViewById(R.id.imageView);
+        bar = (ProgressBar) findViewById(R.id.progressBar);
+        version = (TextView) findViewById(R.id.textView);
+    }
+    private void process() {
+
+        WebSettings websettings = webview.getSettings();
+        websettings.setJavaScriptEnabled(true);
+        websettings.setSaveFormData(false);
+        websettings.setSavePassword(false);
+//if SDK version is greater of 19 then activate hardware acceleration otherwise activate software acceleration
+        if (Build.VERSION.SDK_INT >= 19) {
+            webview.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        } else if (Build.VERSION.SDK_INT >= 11 && Build.VERSION.SDK_INT < 19) {
+            webview.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        }
+        webview.loadUrl(url);
+        webview.setHorizontalScrollBarEnabled(false);
+        webview.setScrollBarStyle(View.SCROLLBARS_OUTSIDE_OVERLAY);
+        webview.setBackgroundColor(128);
+     //   webview.getSettings().setBuiltInZoomControls(true);
+        webview.getSettings().setPluginState(WebSettings.PluginState.ON);
+        webview.getSettings().setAllowFileAccess(true);
+        webview.getSettings().setSupportZoom(true);
+        webview.setWebViewClient(new CustomWebClient());
+        webview.setWebChromeClient(new CustomChromeClient());
+
+        webview.setDownloadListener(new DownloadListener() {
+            public void onDownloadStart(String url, String userAgent,
+                                        String contentDisposition, String mimetype,
+                                        long contentLength) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+
+            }
+        });
+    }
+    public class CustomChromeClient extends WebChromeClient {
+        public boolean onShowFileChooser(
+                WebView webView, ValueCallback<Uri[]> filePathCallback,
+                WebChromeClient.FileChooserParams fileChooserParams) {
+            if(mFilePathCallback != null) {
+                mFilePathCallback.onReceiveValue(null);
+            }
+            mFilePathCallback = filePathCallback;
+
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getApplicationContext().getPackageManager()) != null) {
+                // Create the File where the photo should go
+                File photoFile = null;
+                try {
+                    photoFile = createImageFile();
+                    takePictureIntent.putExtra("PhotoPath", mCameraPhotoPath);
+                } catch (IOException ex) {
+                    // Error occurred while creating the File
+                    Log.e(TAG, "Unable to create Image File", ex);
+                }
+
+                // Continue only if the File was successfully created
+                if (photoFile != null) {
+                    mCameraPhotoPath = "file:" + photoFile.getAbsolutePath();
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                            Uri.fromFile(photoFile));
+                } else {
+                    takePictureIntent = null;
+                }
+            }
+
+            Intent contentSelectionIntent = new Intent(Intent.ACTION_GET_CONTENT);
+            contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE);
+            contentSelectionIntent.setType("image/*");
+
+            Intent[] intentArray;
+            if(takePictureIntent != null) {
+                intentArray = new Intent[]{takePictureIntent};
+            } else {
+                intentArray = new Intent[2];
+            }
+
+            Intent chooserIntent = new Intent(Intent.ACTION_CHOOSER);
+            chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent);
+            chooserIntent.putExtra(Intent.EXTRA_TITLE, "Image Chooser");
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
+
+            startActivityForResult(chooserIntent, INPUT_FILE_REQUEST_CODE);
+
+            return true;
+        }
+    }
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have read or write permission
+        int writePermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int readPermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE);
+        int cameraPermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.CAMERA);
+
+        if (writePermission != PackageManager.PERMISSION_GRANTED || readPermission != PackageManager.PERMISSION_GRANTED || cameraPermission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
+
+    public class CustomWebClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView webview, String url){
+            webview.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onReceivedError(WebView view, int errorCode,
+                                    String description, String failingUrl) {
+            // TODO Auto-generated method stub
+          //  view.loadUrl("file:///android_asset/noconnection.html");
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            visible();
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            unvisible();
+        }
+
+    }
+
+    private void unvisible() {
+
+        webview.setVisibility(View.VISIBLE);
+        logo.setVisibility(View.GONE);
+        bar.setVisibility(View.GONE);
+        version.setVisibility(View.GONE);
+    }
+    private void visible() {
+
+        webview.setVisibility(View.GONE);
+        logo.setVisibility(View.VISIBLE);
+        bar.setVisibility(View.VISIBLE);
+        version.setVisibility(View.VISIBLE);
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "District360_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File imageFile = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        return imageFile;
+    }
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public void onActivityResult (int requestCode, int resultCode, Intent data) {
+        if(requestCode != INPUT_FILE_REQUEST_CODE || mFilePathCallback == null) {
+            super.onActivityResult(requestCode, resultCode, data);
+            return;
+        }
+
+//        Uri[] results = null;
+//
+//        // Check that the response is a good one
+//        if(resultCode == Activity.RESULT_OK) {
+//            if(data == null) {
+//                // If there is not data, then we may have taken a photo
+//                if(mCameraPhotoPath != null) {
+//                    results = new Uri[]{Uri.parse(mCameraPhotoPath)};
+//                }
+//            } else {
+//                String dataString = data.getDataString();
+//                if (dataString != null) {
+//                    results = new Uri[]{Uri.parse(dataString)};
+//                }
+//            }
+//        }
+
+        try {
+            String file_path = mCameraPhotoPath.replace("file:","");
+            File file = new File(file_path);
+            size = file.length();
+
+        }catch (Exception e){
+            Log.e("Error!", "Error while opening image file" + e.getLocalizedMessage());
+        }
+
+        if (data != null || mCameraPhotoPath != null) {
+            Integer count = 0; //fix fby https://github.com/nnian
+            ClipData images = null;
+            try {
+                images = data.getClipData();
+            }catch (Exception e) {
+                Log.e("Error!", e.getLocalizedMessage());
+            }
+
+            if (images == null && data != null && data.getDataString() != null) {
+                count = data.getDataString().length();
+            } else if (images != null) {
+                count = images.getItemCount();
+            }
+            Uri[] results = new Uri[count];
+            // Check that the response is a good one
+            if (resultCode == Activity.RESULT_OK) {
+                if (size != 0) {
+                    // If there is not data, then we may have taken a photo
+                    if (mCameraPhotoPath != null) {
+                        results = new Uri[]{Uri.parse(mCameraPhotoPath)};
+                    }
+                } else if (data.getClipData() == null) {
+                    results = new Uri[]{Uri.parse(data.getDataString())};
+                } else {
+
+                    for (int i = 0; i < images.getItemCount(); i++) {
+                        results[i] = ((ClipData) images).getItemAt(i).getUri();
+                    }
+                }
+            }
+
+            mFilePathCallback.onReceiveValue(results);
+            mFilePathCallback = null;
+            return;
+        }
+
+//        mFilePathCallback.onReceiveValue(results);
+//        mFilePathCallback = null;
+ //       return;
+    }
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webview.canGoBack()) {
+            webview.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+
+        return super.onKeyDown(keyCode, event);
+    }
+}
